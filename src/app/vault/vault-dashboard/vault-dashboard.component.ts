a6f37cc4c76e51786f6bb75e29ecfdd00e142e24
N. Justus <nicola.justus@t-online.de>
2019-12-28 17:38:53 +0100

    Use 'cmd+l' for locking the vault
    And ensure that the locked modal is displayed only once.

--- src/app/vault/vault-dashboard/vault-dashboard.component.ts
+++ src/app/vault/vault-dashboard/vault-dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { vaultAddressKey } from 'app/core/constants';
-import { map, share, first } from 'rxjs/operators';
+import { map, share, first, distinctUntilChanged, filter, flatMap, tap } from 'rxjs/operators';
 import { Observable, Subject, Subscription } from 'rxjs';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { LockedVaultModalComponent } from '../locked-vault-modal/locked-vault-modal.component';
+import Mousetrap from 'mousetrap'
 import { Vault } from '../vault';
 
 @Component({
@@ -23,14 +24,23 @@ export class VaultDashboardComponent implements OnInit, OnDestroy {
 
     this.vaultPath$ = this.activatedRoute.queryParamMap.pipe(
       map(params => params.get(vaultAddressKey)),
-      map(encodedPath => decodeURIComponent(encodedPath)),
-      share())
+      map(encodedPath => decodeURIComponent(encodedPath)))
   }
 
   ngOnInit() {
-    this.lockVault()
-
     this.vault$.subscribe(x => console.log("vault: ", x))
+
+    Mousetrap.bind(['command+l', 'ctrl+l'], () => {
+      this.lockVault()
+    })
+
+    this.locked$.asObservable().pipe(
+      distinctUntilChanged(),
+      filter(b => b),
+      flatMap(_b => this.vaultPath$)
+    ).subscribe(path => this.displayLockedModal(path))
+
+    this.lockVault()
   }
 
   ngOnDestroy() {
@@ -38,18 +48,18 @@ export class VaultDashboardComponent implements OnInit, OnDestroy {
 
   lockVault() {
     this.locked$.next(true)
+  }
 
-    this.vaultPath$.pipe(first()).subscribe(vaultPath => {
-      const options = {
-        ignoreBackdropClick: true,
-        initialState: { vaultPath }
-      }
-      const modalRef = this.modalService.show(LockedVaultModalComponent, options)
+  private displayLockedModal(vaultPath: string) {
+    const options = {
+      ignoreBackdropClick: true,
+      initialState: { vaultPath }
+    }
+    const modalRef = this.modalService.show(LockedVaultModalComponent, options)
 
-      modalRef.content.openedVault$.pipe(first()).subscribe(vault => {
-        this.locked$.next(false)
-        this.vault$.next(vault)
-      })
+    modalRef.content.openedVault$.pipe(first()).subscribe(vault => {
+      this.locked$.next(false)
+      this.vault$.next(vault)
     })
   }
 }
