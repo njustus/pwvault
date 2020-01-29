2969a2474fafe86a6d8c63e7d26496325462b933
N. Justus <nicola.justus@t-online.de>
2020-01-29 19:30:57 +0100

    assign urls to brand icons

--- src/app/core/services/icon-provider.service.ts
+++ src/app/core/services/icon-provider.service.ts
@@ -5,19 +5,20 @@ import { faAt, faDesktop, IconDefinition } from '@fortawesome/free-solid-svg-ico
 export interface IconDescription {
   icon: IconDefinition
   brand: string
+  url?: string
 }
 
 export class IconProviderService {
   public static readonly icons: IconDescription[] = [
-    { icon: faDocker, brand: 'Docker' },
-    { icon: faFacebook, brand: 'Facebook' },
-    { icon: faFirefox, brand: 'Firefox' },
-    { icon: faApple, brand: 'Apple' },
-    { icon: faAtlassian, brand: 'Atlassian' },
-    { icon: faAmazon, brand: 'Amazon' },
-    { icon: faGoogle, brand: 'Google' },
-    { icon: faGithub, brand: 'GitHub' },
-    { icon: faGitlab, brand: 'GitLab' },
+    { icon: faDocker, brand: 'Docker', url: 'https://hub.docker.com' },
+    { icon: faFacebook, brand: 'Facebook', url: 'https://de-de.facebook.com' },
+    { icon: faFirefox, brand: 'Firefox', url: 'https://www.mozilla.org' },
+    { icon: faApple, brand: 'Apple', url: 'https://www.apple.com' },
+    { icon: faAtlassian, brand: 'Atlassian', url: 'https://www.atlassian.com' },
+    { icon: faAmazon, brand: 'Amazon', url: 'https://www.amazon.com' },
+    { icon: faGoogle, brand: 'Google', url: 'https://www.google.com' },
+    { icon: faGithub, brand: 'GitHub', url: 'https://www.github.com' },
+    { icon: faGitlab, brand: 'GitLab', url: 'https://www.gitlab.com' },
     { icon: faAt, brand: 'E-Mail' },
     { icon: faDesktop, brand: 'Computer' },
   ]
@@ -28,5 +29,10 @@ export class IconProviderService {
 
   public static get iconDescriptions(): IconDefinition[] {
     return IconProviderService.icons.map(descr => descr.icon)
+  }
+
+  public static urlForBrand(brand: string): string | undefined {
+    const descr = IconProviderService.icons.find(icon => icon.brand === brand)
+    return descr.url
   }
 }
